25e58d4edf66f7505886f69ec244fe7743fa17f7
KarthiDreamr <karthidreamr@gmail.com>
2023-08-22 22:42:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -0,0 +1,175 @@
+package com.example.exercise2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.exercise2.ui.theme.Exercise2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Exercise2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomePage()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage() {
+    MaterialTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("My App")
+                    }
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /* ... */ }) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                }
+            },
+            bottomBar = {
+                BottomAppBar {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        IconButton(
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Filled.Favorite, contentDescription = null
+                                )
+                            },
+                            onClick = {
+
+                            }
+                        )
+                        IconButton(
+                                content = {
+                                    Icon(
+                                        imageVector = Icons.Filled.Call, contentDescription = null
+                                    )
+                                },
+                        onClick = {
+
+                        }
+                        )
+                        IconButton(
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Filled.Email, contentDescription = null
+                                )
+                            },
+                            onClick = {
+
+                            }
+                        )
+                        IconButton(
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Filled.Send, contentDescription = null
+                                )
+                            },
+                            onClick = {
+
+                            }
+                        )
+                        IconButton(
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Filled.Build, contentDescription = null
+                                )
+                            },
+                            onClick = {
+
+                            }
+                        )
+                    }
+
+                }
+            }
+        ) { contentPadding ->
+
+            Column(
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .padding(horizontal = 17.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                Text(
+                    "Kotlin was replaced as the Official language for Android Development by Google on 2017",
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                Text("Kotlin is a cross-platform, statically typed, general-purpose high-level programming language with type inference. ")
+                Spacer(modifier = Modifier.size(10.dp))
+                Text("Jetpack Compose is a modern toolkit for building native Android UI. Jetpack Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.")
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(
+                    "Advantages of Kotlin over Java",
+                    fontWeight = FontWeight.Bold
+                )
+                Text("1.Conciseness and expressiveness:")
+                Text("2.Safety and reliability")
+                Text("3.Speed and efficiency")
+                Text("4.Modern and innovative")
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Exercise2Theme {
+        HomePage()
+    }
+}
+
+
+// Jetpack Compose is a modern toolkit for building native Android UI. Jetpack Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.
+// Kotlin is a cross-platform, statically typed, general-purpose high-level programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise. Kotlin mainly targets the JVM, but also compiles to JavaScript or native code via LLVM.
